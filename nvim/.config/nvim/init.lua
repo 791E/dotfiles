b51b6a247a5c851bbbe0f67c2f0f20983edcf686
Gian Scherer <g.a.scherer@outlook.com>
2024-08-17 22:55:39 +0000

    Add shortcut to paste into neovim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -4,6 +4,9 @@ vim.cmd("set shiftwidth=4")
 vim.cmd("set softtabstop=4")
 vim.g.mapleader = " "
 
+-- Paste things from outside nvim into nvim with cliphist
+vim.keymap.set("n", "<leader>v", ":r !cliphist list | head -n 1 | cut -d '	' -f 2-<CR>")
+
 -- Setting up lazy.nvim plugin manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
