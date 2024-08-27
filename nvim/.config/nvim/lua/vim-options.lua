3d58afc0f6fb8bd19ca48ef62d76ca13c7b83afc
Gian Scherer <g.a.scherer@outlook.com>
2024-08-27 21:16:56 +0000

    Add vim keybindings

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -4,3 +4,15 @@ vim.cmd("set shiftwidth=4")
 vim.cmd("set softtabstop=4")
 vim.g.mapleader = " "
 
+-- Window navigation
+vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", { noremap = true, silent = true} )
+vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", { noremap = true, silent = true} )
+vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", { noremap = true, silent = true} )
+vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", { noremap = true, silent = true} )
+
+-- Window splitting
+vim.api.nvim_set_keymap("n", "<C-s>", ":split<CR>", { noremap = true, silent = true} )
+vim.api.nvim_set_keymap("n", "<C-v>", ":vsplit<CR>", { noremap = true, silent = true} )
+
+-- Visual Block
+vim.api.nvim_set_keymap("n", "<leader>v", "<C-v>", { noremap = true, silent = true} )
