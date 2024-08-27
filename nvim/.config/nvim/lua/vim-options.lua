c6a861a03449b24e3be0b5ff9c584aa97960b738
Gian Scherer <g.a.scherer@outlook.com>
2024-08-27 21:02:52 +0000

    Remove unnecessary keybinding for pasting for better compatibility

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -4,6 +4,3 @@ vim.cmd("set shiftwidth=4")
 vim.cmd("set softtabstop=4")
 vim.g.mapleader = " "
 
--- Paste things from outside nvim into nvim with cliphist
-vim.keymap.set("n", "<leader>v", ":r !cliphist list | head -n 1 | cut -d '	' -f 2-<CR>")
-
