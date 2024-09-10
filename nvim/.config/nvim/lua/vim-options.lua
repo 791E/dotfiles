c523427e9cc042b8abe9539d596dd92d93415f26
Gian Scherer <g.a.scherer@outlook.com>
2024-09-10 10:50:20 +0000

    Add keymap for write, enable line numbers

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -15,7 +15,13 @@ vim.api.nvim_set_keymap("n", "<C-s>", ":split<CR>", { noremap = true, silent = t
 vim.api.nvim_set_keymap("n", "<C-v>", ":vsplit<CR>", { noremap = true, silent = true} )
 
 -- Close window
-vim.api.nvim_set_keymap("n", "<C-q>", ":q<CR>+", { noremap = true, silent = true } )
+vim.api.nvim_set_keymap("n", "<C-q>", ":q<CR>", { noremap = true, silent = true } )
+
+-- Write file
+vim.api.nvim_set_keymap("n", "<C-w>", ":w<CR>", { noremap = true, silent = true } )
 
 -- Visual Block
 vim.api.nvim_set_keymap("n", "<leader>v", "<C-v>", { noremap = true, silent = true} )
+
+-- Enable line numbers
+vim.cmd("set number")
