018b0368dc571d740065b9eb0bb37ce3c1ac5b2c
Gian Scherer <g.a.scherer@outlook.com>
2024-09-26 17:53:46 +0000

    Add keybindings for quitting / writing files in nvim (improve formatting)

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -5,23 +5,29 @@ vim.cmd("set softtabstop=4")
 vim.g.mapleader = " "
 
 -- Window navigation
-vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", { noremap = true, silent = true} )
-vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", { noremap = true, silent = true} )
-vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", { noremap = true, silent = true} )
-vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", { noremap = true, silent = true} )
+vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", { noremap = true, silent = true } )
+vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", { noremap = true, silent = true } )
+vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", { noremap = true, silent = true } )
+vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", { noremap = true, silent = true } )
 
 -- Window splitting
-vim.api.nvim_set_keymap("n", "<C-s>", ":split<CR>", { noremap = true, silent = true} )
-vim.api.nvim_set_keymap("n", "<C-v>", ":vsplit<CR>", { noremap = true, silent = true} )
+vim.api.nvim_set_keymap("n", "<C-s>", ":split<CR>", { noremap = true, silent = true } )
+vim.api.nvim_set_keymap("n", "<C-v>", ":vsplit<CR>", { noremap = true, silent = true } )
 
 -- Close window
 vim.api.nvim_set_keymap("n", "<C-q>", ":q<CR>", { noremap = true, silent = true } )
 
+-- Force close window
+vim.api.nvim_set_keymap("n", "<C-M-q>", ":q!<CR>", { noremap = true, silent = true })
+
 -- Write file
 vim.api.nvim_set_keymap("n", "<C-w>", ":w<CR>", { noremap = true, silent = true } )
 
+-- Write-quit all
+vim.api.nvim_set_keymap("n", "<C-wq>", ":wqa<CR>", { noremap = true, silent = true })
+
 -- Visual Block
-vim.api.nvim_set_keymap("n", "<leader>v", "<C-v>", { noremap = true, silent = true} )
+vim.api.nvim_set_keymap("n", "<leader>v", "<C-v>", { noremap = true, silent = true } )
 
 -- Enable line numbers
 vim.cmd("set number")
