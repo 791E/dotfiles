cf1ab054bee2112b33c66d45b243af8342b9f118
Gian Scherer <g.a.scherer@outlook.com>
2024-09-30 20:17:14 +0200

    Make LSP-related keybindings actually make sense, add noremap and silent

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -21,9 +21,9 @@ return {
 			lspconfig.clangd.setup({})
 			lspconfig.pyright.setup({})
 
-			vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-			vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+			vim.keymap.set('n', '<leader>h', vim.lsp.buf.hover, { noremap = true, silent = true })
+			vim.keymap.set('n', '<leader>d', vim.lsp.buf.definition, { noremap = true, silent = true })
+			vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, { noremap = true, silent = true })
 		end
 	}
 }
