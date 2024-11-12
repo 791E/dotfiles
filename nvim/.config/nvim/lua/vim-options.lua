8c3b64f38a0365490cac53f2981ec72764f00b2d
Gian Scherer <g.a.scherer@outlook.com>
2024-11-12 18:10:23 +0000

    Change keybindings

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -11,23 +11,20 @@ vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", { noremap = true, silent = true 
 vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", { noremap = true, silent = true } )
 
 -- Window splitting
-vim.api.nvim_set_keymap("n", "<C-s>", ":split<CR>", { noremap = true, silent = true } )
-vim.api.nvim_set_keymap("n", "<C-v>", ":vsplit<CR>", { noremap = true, silent = true } )
+vim.api.nvim_set_keymap("n", "<leader>s", ":split<CR>", { noremap = true, silent = true } )
+vim.api.nvim_set_keymap("n", "<leader>v", ":vsplit<CR>", { noremap = true, silent = true } )
 
 -- Close window
-vim.api.nvim_set_keymap("n", "<C-q>", ":q<CR>", { noremap = true, silent = true } )
+vim.api.nvim_set_keymap("n", "<leader>q", ":q<CR>", { noremap = true, silent = true } )
 
 -- Force close window
-vim.api.nvim_set_keymap("n", "<C-M-q>", ":q!<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader><C-q>", ":q!<CR>", { noremap = true, silent = true })
 
 -- Write file
-vim.api.nvim_set_keymap("n", "<C-w>", ":w<CR>", { noremap = true, silent = true } )
+vim.api.nvim_set_keymap("n", "<leader>w", ":w<CR>", { noremap = true, silent = true } )
 
 -- Write-quit all
-vim.api.nvim_set_keymap("n", "<C-wq>", ":wqa<CR>", { noremap = true, silent = true })
-
--- Visual Block
-vim.api.nvim_set_keymap("n", "<leader>v", "<C-v>", { noremap = true, silent = true } )
+vim.api.nvim_set_keymap("n", "<leader>wq", ":wqa<CR>", { noremap = true, silent = true })
 
 -- Enable line numbers at the current line
 vim.cmd("set number")
@@ -36,7 +33,7 @@ vim.cmd("set number")
 vim.cmd("set relativenumber")
 
 -- Toggle neo-tree filesystem
-vim.keymap.set("n", "<C-n>", ":Neotree filesystem left toggle<CR>")
+vim.keymap.set("n", "<leader>n", ":Neotree filesystem left toggle<CR>")
 
 -- Keybindings for LSP related stuff
 vim.keymap.set('n', '<leader>h', vim.lsp.buf.hover, { noremap = true, silent = true })
