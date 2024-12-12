148fd354272b45a48f92a0c2f8d5a8d46e3a4d74
Gian Scherer <g.a.scherer@outlook.com>
2024-12-12 13:43:40 +0100

    Remove duplicate vim options from init.lua (they are in lua/vim-options.lua)

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,9 +1,3 @@
--- Basic vim settings
-vim.cmd("set tabstop=4")
-vim.cmd("set shiftwidth=4")
-vim.cmd("set softtabstop=4")
-vim.g.mapleader = " "
-
 -- Paste things from outside nvim into nvim with cliphist
 vim.keymap.set("n", "<leader>v", ":r !cliphist list | head -n 1 | cut -d '	' -f 2-<CR>")
 
