d2ca73eb61115db12a50ef0c576932c3a0634c10
Gian Scherer <g.a.scherer@outlook.com>
2025-02-16 11:10:13 +0000

    Use lsd instead of ls for listing everything

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -2,9 +2,9 @@
 # Zsh Aliases #
 ###############
 
+alias lsa='lsd -AF1'
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
-alias lsa='ls -A --color=auto'
 alias svim='sudo -E nvim'
 alias cfzf='cd $(dirname $(fzf))'
 alias nfzf='nvim $(fzf)'
