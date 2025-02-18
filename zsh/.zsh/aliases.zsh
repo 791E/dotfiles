4a34581fdf0d71b41e6cd76de8c9ab3e3f9f0582
Gian Scherer <791e@proton.me>
2025-02-18 09:56:03 +0100

    Add zsh alias for directory tree view

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -4,6 +4,7 @@
 
 alias lsa='lsd -AF1'
 alias ls='ls --color=auto'
+alias lst='lsd -AF --tree --ignore-glob .git'
 alias grep='grep --color=auto'
 alias svim='sudo -E nvim'
 alias cfzf='cd $(dirname $(fzf))'
