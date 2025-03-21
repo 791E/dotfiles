55cec7c69043566a5e0f65d6d670b37e4b2d4deb
Gian Scherer <791e@proton.me>
2025-03-21 14:26:39 +0100

    Automatic updates, create miscellaneous plugins file, install yuck.vim and parinfer-rust for eww configuring

---
+++ nvim/.config/nvim/lua/plugins/misc-plugins.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        "nvim-tree/nvim-web-devicons",
+        opts = {},
+    },
+    {
+        "elkowar/yuck.vim",
+        config = function() end,
+    },
+    {
+        "eraserhd/parinfer-rust",
+        build = "cargo build --release",
+    },
+}
