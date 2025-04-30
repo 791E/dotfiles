6b4499eadb46a66b9ef7d4ae8bac24d1e7af8802
Gian Scherer <791e@proton.me>
2025-04-30 10:47:07 +0200

    Add comments for each plugin in misc-plugins.lua

--- nvim/.config/nvim/lua/plugins/misc-plugins.lua
+++ nvim/.config/nvim/lua/plugins/misc-plugins.lua
@@ -1,12 +1,15 @@
 return {
+    -- devicons for neotree
     {
         "nvim-tree/nvim-web-devicons",
         opts = {},
     },
+    -- syntax highlighting for eww.yuck
     {
         "elkowar/yuck.vim",
         config = function() end,
     },
+    -- simplify working with Lisp-type languages
     {
         "eraserhd/parinfer-rust",
         build = "cargo build --release",
