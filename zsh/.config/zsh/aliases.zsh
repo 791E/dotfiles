1fa0c128b8503d265866e80fb0f35c748d5f8d38
Gian Scherer <791e@proton.me>
2025-05-07 10:55:08 +0200

    Remove unneeded alias, improve alias, add new alias

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -7,10 +7,10 @@ alias ls='ls --color=auto'
 alias lst='lsd -AF --tree --ignore-glob .git --ignore-glob target'
 alias grep='grep --color=auto'
 alias svim='sudo -E nvim'
-alias cfzf='cd $(dirname $(ff))'
-alias nfzf='nvim $(ff)'
+alias nff='nvim $(ff)'
 alias ff='fzf --preview "bat --color=always --style=numbers {}"'
 alias lg='lazygit'
+alias cla='clear && lsa'
 
 # compress pdf files with ghostscript
 pdf_compress ()
